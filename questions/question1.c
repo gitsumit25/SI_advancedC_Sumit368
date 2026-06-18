9a40ff411aa4c9d699d7977f68ba7e44e1f807e1
Sumit Chahar <sumitchahar0912@gmail.com>
2026-06-18 20:57:43 +0530

    Initial commit

---
+++ questions/question1.c
@@ -0,0 +1,7 @@
+// WAP TO PRINT " HELLO WORLD"
+
+#include <stdio.h>
+int main () { 
+printf("hello world");
+return 0;
+}
---
+++ questions/question5.c
@@ -0,0 +1,10 @@
+//Celsius to Fahrenheit 
+#include <stdio.h>
+int main () {
+float c , f ;
+printf(" enter you temperature in celsius: ");
+scanf("%f",&c);
+f= (c*1.8)+32 ;
+printf("your temp in fahrenheit is %f",f);
+return 0;
+}
